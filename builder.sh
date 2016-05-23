a9d45af74568f5f74bb50badc46a208a45bbe64c
Jacob Gadikian <faddat@gmail.com>
2016-05-24 05:44:20 +0700

    fixed sudo stuff

--- builder.sh
+++ builder.sh
@@ -11,25 +11,24 @@ make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
 cd ../u-boot
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- orangepi_pc_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
-sudo -s
-dd if=/dev/zero of=~/opi-arch-fresh.img bs=1024 count=1048576
+sudo dd if=/dev/zero of=~/opi-arch-fresh.img bs=1024 count=1048576
 export card=/dev/loop1
-dd if=/dev/zero of=${card} bs=1k count=1023 seek=1
-dd if=~/opi-diskimage-arch/u-boot/u-boot-sunxi-with-spl.bin of=${card} bs=1024 seek=8
-losetup /dev/loop1 ~/myDisk.img
-fdisk -u -p /dev/loop1 <<EOF
+sudo dd if=/dev/zero of=${card} bs=1k count=1023 seek=1
+sudo dd if=~/opi-diskimage-arch/u-boot/u-boot-sunxi-with-spl.bin of=${card} bs=1024 seek=8
+sudo losetup /dev/loop1 ~/myDisk.img
+sudo fdisk -u -p /dev/loop1 <<EOF
 n
 p
 1
 
 w
 EOF
-kpartx -v -a /dev/loop1
-mkfs.ext4 /dev/mapper/loop1p1
-mount /dev/mapper/loop1p1 $HOME/opi-diskimage-arch/imagemount
-wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
-bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C $HOME/opi-diskimage-arch/imagemount
-sync
+sudo kpartx -v -a /dev/loop1
+sudo mkfs.ext4 /dev/mapper/loop1p1
+sudo mount /dev/mapper/loop1p1 $HOME/opi-diskimage-arch/imagemount
+sudo wget http://archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
+sudo bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C $HOME/opi-diskimage-arch/imagemount
+sudo sync
 
 
 
